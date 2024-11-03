011cf39f77a58f66ff520bee8884de17103bc579
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2024-11-03 19:24:26 +0900

    Initial commit

---
+++ server/src/main/java/com/ssafy/living_spot/LivingSpotApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.living_spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingSpotApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ssafy/living_spot/LivingSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.living_spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivingSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
